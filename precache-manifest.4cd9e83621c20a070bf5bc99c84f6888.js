76ac8689e124b515940ddfec534367e5893b15ef
IkennaU <pacificheights2008@hotmail.com>
2019-05-14 22:35:19 +0100

    Updates

---
+++ precache-manifest.4cd9e83621c20a070bf5bc99c84f6888.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "59782e1044de3948c2ff",
+    "url": "/contactmanager2/static/css/main.4f646855.chunk.css"
+  },
+  {
+    "revision": "59782e1044de3948c2ff",
+    "url": "/contactmanager2/static/js/main.59782e10.chunk.js"
+  },
+  {
+    "revision": "e5db00dc8acccfae669e",
+    "url": "/contactmanager2/static/css/1.ad28ae2f.chunk.css"
+  },
+  {
+    "revision": "e5db00dc8acccfae669e",
+    "url": "/contactmanager2/static/js/1.e5db00dc.chunk.js"
+  },
+  {
+    "revision": "4c244971e56952fb0ba7",
+    "url": "/contactmanager2/static/js/runtime~main.4c244971.js"
+  },
+  {
+    "revision": "72698f37033aae617ccae7f06af3d869",
+    "url": "/contactmanager2/index.html"
+  }
+];
